9ea88c1353e184339ee613abfbfbc93de0c02c59
TheMat556 <79401695+TheMat556@users.noreply.github.com>
2024-09-01 00:43:03 +0200

    replace prettier and eslint with biomejs

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -4,22 +4,16 @@ const TotalBalanceBox = ({
   totalCurrentBalance,
 }: TotlaBalanceBoxProps) => {
   return (
-    <section className="total-balance">
-      <div className="total-balance-chart">
-        {/*Donut Chart*/}
-      </div>
-      <div className="flex flex-col gap-6">
-        <h2 className="header-2">
+    <section className='total-balance'>
+      <div className='total-balance-chart'>{/*Donut Chart*/}</div>
+      <div className='flex flex-col gap-6'>
+        <h2 className='header-2'>
           Bank Accounts:
           {totalBanks}
         </h2>
-        <div className="flex flex-col gap-2">
-          <p className="total-balance-label">
-            Total Current Balance
-          </p>
-          <p>
-            Total Balance Amou1t
-          </p>
+        <div className='flex flex-col gap-2'>
+          <p className='total-balance-label'>Total Current Balance</p>
+          <p>Total Balance Amout</p>
         </div>
       </div>
     </section>
