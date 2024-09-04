728f6e3518c370664e064b9657ab855a13363323
TheMat556 <79401695+TheMat556@users.noreply.github.com>
2024-09-04 22:30:21 +0200

    auth ui

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+'use server'
+
+export const signIn = async () => {
+  try {
+    //Mutation / database
+  } catch(error) {
+    console.log('Error',error);
+  }
+}
+
+export const signUp = async (userData: SignUpParams) => {
+  try {
+    //Mutation / database
+  } catch(error) {
+    console.log('Error',error);
+  }
+}
